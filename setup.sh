5aac0b323f8ac321abb05f82dbfba788d237f1df
David Phillips <zantier.tasa@gmail.com>
2022-12-02 11:01:35 +0000

    Add setup.sh, for setting up symlinks on linux

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# After cloning this repo to ~/.dotfiles, run this to create symlinks
+
+files=(
+    .config/nvim/init.lua
+    .gitconfig
+    .githooks/pre-commit
+    .inputrc
+    .my.sh
+    .my.vim
+    .mybase.vim
+    .mytiny.vim
+)
+
+mkdir -p ../.config/nvim
+mkdir -p ../.githooks
+for path in ${files[@]}; do
+    # Change path into the format "../../"
+    up=$(echo $path | sed -E -e 's/[^/]*$//g' -e 's/[^/]*\//..\//g')
+    ln -s $up.dotfiles/$path ../$path
+done
+echo Created symlinks
