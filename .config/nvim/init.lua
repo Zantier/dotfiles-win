1eb3ebfc4fafc0a00e1f6b8002ff490443b75e6d
David Phillilps <zantier.tasa@gmail.com>
2022-12-02 02:25:26 +0000

    Add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,39 @@
+vim.cmd([[
+call plug#begin('~/.vim/plugged')
+Plug 'easymotion/vim-easymotion'
+Plug 'tpope/vim-surround'
+Plug 'neovim/nvim-lspconfig'
+Plug 'junegunn/fzf'
+Plug 'junegunn/fzf.vim'
+call plug#end()
+
+source ~/.mytiny.vim
+
+autocmd FileType css setlocal expandtab ts=2 sts=2 sw=2
+autocmd FileType html setlocal expandtab ts=2 sts=2 sw=2
+autocmd FileType javascript setlocal expandtab ts=2 sts=2 sw=2
+" For python files, vim sets expandtab ts=8 sts=4 sw=4
+autocmd FileType python setlocal ts=4
+
+let g:EasyMotion_do_mapping = 0
+let g:EasyMotion_re_anywhere = '\v<[A-Za-z0-9]|(^|[ \t]+)@<=[^ \tA-Za-z0-9]([^A-Za-z0-9]|$){4}'
+set omnifunc=v:lua.vim.lsp.omnifunc
+
+
+" Mappings
+"---------
+" Turn off search highlight and redraw screen
+nnoremap <C-L> :nohl<CR><C-L>
+nmap ,f <Plug>(easymotion-jumptoanywhere)
+vmap ,f <Plug>(easymotion-jumptoanywhere)
+nmap gf :Files<cr>
+inoremap <c-space> <c-x><c-o>
+]])
+
+require'lspconfig'.pyright.setup{}
+vim.api.nvim_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition)
+vim.keymap.set('n', ',i', vim.lsp.buf.hover)
