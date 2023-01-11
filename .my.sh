2362a88fb55267d8ccd76380e601d854a8a3d2ef
David Phillips <zantier.tasa@gmail.com>
2023-01-11 18:59:24 +0000

    bashrc: Remove unused code

--- .my.sh
+++ .my.sh
@@ -84,7 +84,6 @@ function kind() {(
 function ranger-cd {
     tempfile="$(mktemp -t tmp.XXXXXX)"
     /usr/bin/ranger --choosedir="$tempfile" "${@:-$(pwd)}"
-    test -f "$tempfile" &&
     if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
         cd -- "$(cat "$tempfile")"
     fi
