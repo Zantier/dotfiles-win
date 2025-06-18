c576f6b412da47341b81261c03c3364973313b35
David Phillips <zantier.tasa@gmail.com>
2025-06-18 21:58:21 +0100

    Rename home.nix to home_common.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -36,7 +36,7 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ ./home_common.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
