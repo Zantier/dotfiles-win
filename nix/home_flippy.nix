19c499261aaff3d4b1264406425879589c7ac37b
David Phillips <zantier.tasa@gmail.com>
2025-06-18 23:23:40 +0100

    nix: Remove repeated text from flippy .bashrc

--- nix/home_flippy.nix
+++ nix/home_flippy.nix
@@ -25,19 +25,8 @@
   ];
 
   programs.bash.initExtra = ''
-    # 47 light gray, 102 light green, 103 light yellow, 104 light blue, 105 light magenta, 106 light cyan, 107 white
-    prompt_color="105"
-    alias ggmail='git init;git config user.name "Dave Phillips";git config user.email zantier.tasa@gmail.com'
-    alias gdot='git init;git config user.name "Dave Phillips";git config user.email david@dottxt.co'
-    source ~/.my.sh
     export ANDROID_HOME="/z/android-sdk"
     export PNPM_HOME="/z/pnpm"
     export PATH="$PNPM_HOME:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
-    export NNN_FIFO=/tmp/nnn.fifo
-    export NNN_TMPFILE=/tmp/nnn.lastd
-    alias z='nnn-cd /z'
-    py() {
-      nix-shell -p python311 --run "python $@"
-    }
   '';
 }
